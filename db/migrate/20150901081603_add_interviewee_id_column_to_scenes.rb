49c7c6e39907688c5d8b52630d30fe9c9018abab
Yhoon Kim <yhoonkim01@gmail.com>
2015-09-03 15:14:21 +0900

    fix migration error(reset_column_information)

--- db/migrate/20150901081603_add_interviewee_id_column_to_scenes.rb
+++ db/migrate/20150901081603_add_interviewee_id_column_to_scenes.rb
@@ -1,6 +1,8 @@
 class AddIntervieweeIdColumnToScenes < ActiveRecord::Migration
   def up
     add_column :scenes, :interviewee_id, :integer
+    Scene.reset_column_information
+
     Interviewee.all.each do |i|
       scene = Scene.create(:description => "#{i.name}의 첫번째 씬", interviewee_id: i.id)
       i.lines.update_all(scene_id: scene.id)
