f8bedbae9df9ce499d334b13f6307e91c9a86a16
Junhee Woo <junhee.woo@gmail.com>
2015-10-25 19:37:28 +0900

    Add OG tag
    * Need to update when we gen facebook app id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,17 @@ module ApplicationHelper
 
   end
 
+  def generate_og_tags
+
+    meta_image = "<meta property=\"og:image\" content=\"#{request.protocol}#{request.host_with_port}#{asset_path("prismy_meta_image.png")}\" />"
+    meta_title = "<meta property=\"og:title\" content=\"프리즈미(Prismy)\" />"
+    meta_description = "<meta property=\"og:description\" content=\"여러가지 색깔을 가진 그들의 이야기를 들어보세요.\" />"
+
+    meta_type = "<meta property=\"og:type\" content=\"website\" />"
+    meta_site_name = "<meta property=\"og:site_name\" content=\"프리즈미(Prismy)\" />"
+    meta_url = "<meta property=\"og:url\" content=\"#{request.original_url}\" />"
+
+    meta_app_id = "<meta property=\"fb:app_id\" content=\"\" />"
+    meta_title + meta_site_name + meta_url + meta_image + meta_description + meta_app_id + meta_type
+  end
 end
