3c6cef251c2487587661667097d613bb01bf3dff
Younghoon Kim <younghoonkim@younghoons-mbp.attlocal.net>
2015-11-29 07:52:21 -0800

    fix: show button

--- app/assets/javascripts/interviewees.js
+++ app/assets/javascripts/interviewees.js
@@ -116,7 +116,7 @@ $(document).on('ready page:load', function () {
             });
           });
 
-          
+
         }
 
       });
@@ -185,8 +185,23 @@ $(document).on('ready page:load', function () {
   {
     if(selectButtonInformation.length == 0)
     {
-      var buttons = "<button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"0\" id=\"btn1\" style=\"visibility:hidden;\">선택지 1</button><button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"1\" id=\"btn2\">다음</button><button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"2\" id=\"btn3\" style=\"visibility:hidden;\">선택지 2</button>";
-      $('#user_answer_div').html(buttons);
+      var buttons = "<button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"0\" id=\"btn1\" style=\"visibility:hidden;\">선택지 1</button>"
+                    + "<button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"1\" id=\"btn2\">다음</button>"
+                    + "<button type=\"button\" class=\"btn btn-primary btn-lg-rect btn-rect btn-block user-answer-btn\" data-sequence=\"2\" id=\"btn3\" style=\"visibility:hidden;\">선택지 2</button>";
+
+      $('#user_answer_div').delay(100).fadeOut(1000, function(){
+        // $('#user_answer_div').children().remove();
+        // $('#user_answer_div').append(buttonString);
+        $('#user_answer_div').html(buttons);
+        $('#userActions-container').hide().delay(100).fadeIn(1200, 'easeInQuad', function(){
+        $('#user_answer_div').delay(100).fadeIn(500, 'easeInQuad');
+        });
+
+        // showSelectButtons();
+      });
+
+
+
     }
     // 주관식 답에 대해서 form을 만들어줘야 할 때
     else if(selectButtonInformation.length == 1 && selectButtonInformation[0].content == "")
